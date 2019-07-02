e09f761f837ba7c6c2bfa223a9ed9e987b6260be
nathanona <nathan.ona@outlook.com>
2019-07-02 17:57:01 -0400

    Comments to the Birthday App

--- Module03/Brithday_App.py
+++ Module03/Brithday_App.py
@@ -7,6 +7,7 @@ def header():
     print('   BIRTHDAY APP            ')
     print('---------------------------')
     print()
+# Asks user for their birthday
 
 
 def day_of_birth():
@@ -16,12 +17,14 @@ def day_of_birth():
     year = int(input("Year: "))
     birth = datetime.date(year, month, day)
     return birth
+# Calculates the number of days from the current day until the user's birthday
 
 
 def days_btwn_dates(ini_date, end_date):
     this_year = datetime.date(end_date.year, ini_date.month, ini_date.day)
-    time_until= this_year - end_date
+    time_until = this_year - end_date
     return time_until.days
+# Outputs a message to the user depending on the time from the current date to his/her birthday
 
 
 def birthday_info(days):
